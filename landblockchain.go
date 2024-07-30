96581478a05ce490f93f76ff4d26d7465ba63c0c
Philip <oumaphilip01@gmil.com>
2024-07-30 23:08:37 +0300

    error: changed cors settings

--- landblockchain.go
+++ landblockchain.go
@@ -1,121 +1,121 @@
-package main
+ package main
 
-import (
-	"crypto/sha256"
-	"encoding/json"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-)
+// import (
+// 	"crypto/sha256"
+// 	"encoding/json"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// 	"time"
+// )
 
-// The Owner information
-type Owner struct {
-	Name, Address, NextOfKin string
-	Id                       string
-	Contact                  int
-}
+// // The Owner information
+// type Owner struct {
+// 	Name, Address, NextOfKin string
+// 	Id                       string
+// 	Contact                  int
+// }
 
-// Title deed information
-type LandInfo struct {
-	LandSize                   float64
-	LandLocation, Restrictions string
-	Boundaries                 [][]int
-}
+// // Title deed information
+// type LandInfo struct {
+// 	LandSize                   float64
+// 	LandLocation, Restrictions string
+// 	Boundaries                 [][]int
+// }
 
-// LandBlock information
-type LandBlock struct {
-	Data         map[string]interface{}
-	TitleDeedId  string
-	TimeStamp    time.Time
-	Hash         string
-	PreviousHash string
-	Pow          int
-}
+// // LandBlock information
+// type LandBlock struct {
+// 	Data         map[string]interface{}
+// 	TitleDeedId  string
+// 	TimeStamp    time.Time
+// 	Hash         string
+// 	PreviousHash string
+// 	Pow          int
+// }
 
-// Custom Blockchain
-type Blockchain struct {
-	genesisBlock LandBlock
-	Chain        []LandBlock
-	Difficulty   int
-}
+// // Custom Blockchain
+// type Blockchain struct {
+// 	genesisBlock LandBlock
+// 	Chain        []LandBlock
+// 	Difficulty   int
+// }
 
-// Generating the hash of a block
-func (b LandBlock) CalculateHash() string {
-	// convert the Data field to json for easy conversion to string
-	dataBytes, err := json.Marshal(b.Data)
-	if err != nil {
-		fmt.Println("Error converting data to json/n", err)
-		os.Exit(1)
+// // Generating the hash of a block
+// func (b LandBlock) CalculateHash() string {
+// 	// convert the Data field to json for easy conversion to string
+// 	dataBytes, err := json.Marshal(b.Data)
+// 	if err != nil {
+// 		fmt.Println("Error converting data to json/n", err)
+// 		os.Exit(1)
 
-	}
+// 	}
 
-	blockData := b.PreviousHash + string(dataBytes) + b.TimeStamp.String() + strconv.Itoa(b.Pow)
-	blockHash := sha256.Sum256([]byte(blockData))
+// 	blockData := b.PreviousHash + string(dataBytes) + b.TimeStamp.String() + strconv.Itoa(b.Pow)
+// 	blockHash := sha256.Sum256([]byte(blockData))
 
-	return fmt.Sprintf("%x", blockHash)
-}
+// 	return fmt.Sprintf("%x", blockHash)
+// }
 
-// Mining new block
-func (b *LandBlock) Mine(difficulty int) {
-	for !strings.HasPrefix(b.Hash, strings.Repeat("0", difficulty)) {
-		b.Pow++
-		b.Hash = b.CalculateHash()
-	}
-}
+// // Mining new block
+// func (b *LandBlock) Mine(difficulty int) {
+// 	for !strings.HasPrefix(b.Hash, strings.Repeat("0", difficulty)) {
+// 		b.Pow++
+// 		b.Hash = b.CalculateHash()
+// 	}
+// }
 
-// Creating the genesis blockchain
-func CreateBlockChain(difficulty int) Blockchain {
-	genesisBlock := LandBlock{
-		Hash:      "0",
-		TimeStamp: time.Now(),
-	}
-	return Blockchain{
-		genesisBlock,
-		[]LandBlock{genesisBlock},
-		difficulty,
-	}
-}
+// // Creating the genesis blockchain
+// func CreateBlockChain(difficulty int) Blockchain {
+// 	genesisBlock := LandBlock{
+// 		Hash:      "0",
+// 		TimeStamp: time.Now(),
+// 	}
+// 	return Blockchain{
+// 		genesisBlock,
+// 		[]LandBlock{genesisBlock},
+// 		difficulty,
+// 	}
+// }
 
-//Adding newblocks to the blockchain
+// //Adding newblocks to the blockchain
 
-func (b Blockchain) AddBlock(to, from string, amount float64) {
-	landBlockData := map[string]interface{}{
-		"from" : from,
-		"to" : to,
-		"amount" : amount,
-	}
-	lastBlock := b.Chain[len(b.Chain)-1]
-	newBlock := LandBlock{
-		Data : landBlockData,
-		PreviousHash : lastBlock.Hash,
-		TimeStamp : time.Now(),
-	}
-	newBlock.Mine(b.Difficulty)
-	b.Chain = append(b.Chain, newBlock)
-}
+// func (b Blockchain) AddBlock(to, from string, amount float64) {
+// 	landBlockData := map[string]interface{}{
+// 		"from" : from,
+// 		"to" : to,
+// 		"amount" : amount,
+// 	}
+// 	lastBlock := b.Chain[len(b.Chain)-1]
+// 	newBlock := LandBlock{
+// 		Data : landBlockData,
+// 		PreviousHash : lastBlock.Hash,
+// 		TimeStamp : time.Now(),
+// 	}
+// 	newBlock.Mine(b.Difficulty)
+// 	b.Chain = append(b.Chain, newBlock)
+// }
 
-//Checking the validity of a blockchain
-func (b Blockchain) IsValid()bool{
-	for i := range b.Chain[1:]{
-		currentBlock := b.Chain[i +1]
-		PreviousBlock := b.Chain[i]
-		if currentBlock.Hash != currentBlock.CalculateHash() || currentBlock.Hash != PreviousBlock.Hash{
-			return false
-		}
+// //Checking the validity of a blockchain
+// func (b Blockchain) IsValid()bool{
+// 	for i := range b.Chain[1:]{
+// 		currentBlock := b.Chain[i +1]
+// 		PreviousBlock := b.Chain[i]
+// 		if currentBlock.Hash != currentBlock.CalculateHash() || currentBlock.Hash != PreviousBlock.Hash{
+// 			return false
+// 		}
 
-	}
-	return true
-}
+// 	}
+// 	return true
+// }
 
-//Main func
-func main(){
-	//Create a Blockchain instance with a mining difficulty of 2
-	blockchain := CreateBlockChain(2)
+// //Main func
+// func main(){
+// 	//Create a Blockchain instance with a mining difficulty of 2
+// 	blockchain := CreateBlockChain(2)
 
-	//Record Transactions on the blockchain
-	blockchain.AddBlock("Elijah", "Ben", 1)
-	blockchain.AddBlock("Nick", "Stee", 5)
-	fmt.Println(blockchain.IsValid())
-}
+// 	//Record Transactions on the blockchain
+// 	blockchain.AddBlock("Elijah", "Ben", 1)
+// 	blockchain.AddBlock("Nick", "Stee", 5)
+// 	fmt.Println(blockchain.IsValid())
+// }
--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"handlers/handlers"
+
+	"ardhichain/handlers"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handlers.IndexHandler)
-	http.HandleFunc("/search", handlers.IndexHandler)
+	http.HandleFunc("/search", handlers.SearchHandler)
 	http.HandleFunc("/verify", handlers.VerifyHandler)
 	http.HandleFunc("/new-owner", handlers.NewOwnerHandler)
 
@@ -27,4 +28,16 @@ func main() {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
